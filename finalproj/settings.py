0a5e676aa7e4fc6947693d8cd89d98ebcbaf0129
dvine21 <divineangelpialago671@gmail.com>
2025-06-02 22:53:27 +0800

    Prepare for deployment

--- finalproj/settings.py
+++ finalproj/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-)%b@o&!k_&%=^ihcx$vmx6m1ii(=5po-8=czf*zir5cdm4h$xj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['studentinfo.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -105,8 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'schoolapp' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
